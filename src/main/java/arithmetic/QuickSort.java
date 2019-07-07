3f4b636bcf435e880abc8cd71591db32515784c4
dxb350352 <dxb350352@163.com>
2019-07-07 20:44:54 +0800

    小改

--- src/main/java/arithmetic/QuickSort.java
+++ src/main/java/arithmetic/QuickSort.java
@@ -1,41 +1,32 @@
 package arithmetic;
 
-import java.util.Arrays;
-
 public class QuickSort {
-    public static int[] qsort(int arr[], int start, int end) {
-        int pivot = arr[start];
-        int i = start;
-        int j = end;
-        while (i < j) {
-            while ((i < j) && (arr[j] > pivot)) {
-                j--;
+    public static int[] quick_sort(int arr[], int l, int r) {
+        if (l < r) {
+            //Swap(arr[l], arr[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+            int i = l, j = r, x = arr[l];
+            while (i < j) {
+                while (i < j && arr[j] >= x) // 从右向左找第一个小于x的数
+                    j--;
+                if (i < j)
+                    arr[i++] = arr[j];
+
+                while (i < j && arr[i] < x) // 从左向右找第一个大于等于x的数
+                    i++;
+                if (i < j)
+                    arr[j--] = arr[i];
             }
-            while ((i < j) && (arr[i] < pivot)) {
-                i++;
-            }
-            if ((arr[i] == arr[j]) && (i < j)) {
-                i++;
-            } else {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+            arr[i] = x;
+            quick_sort(arr, l, i - 1); // 递归调用
+            quick_sort(arr, i + 1, r);
         }
-        if (i - 1 > start) {
-            arr = qsort(arr, start, i - 1);
-        }
-        if (j + 1 < end) {
-            arr = qsort(arr, j + 1, end);
-        }
-        System.out.println(Arrays.toString(arr));
-        return (arr);
+        return arr;
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{9, 8, 7, 1, 6, 5, 4, 3, 2, 10};
         int len = arr.length - 1;
-        qsort(arr, 0, len);
+        quick_sort(arr, 0, len);
     }
 
 }
