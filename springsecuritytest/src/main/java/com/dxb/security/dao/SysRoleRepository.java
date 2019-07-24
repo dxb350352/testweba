f8183d6e170d2f9c0350445c84cec2a6137da156
戴晓波 <daixiaobo@wondersoft.cn>
2019-07-24 13:51:37 +0800

    spring security搬出来

--- springsecuritytest/src/main/java/com/dxb/security/dao/SysRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dxb.security.dao;
-
-import com.dxb.security.entity.SysRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SysRoleRepository extends JpaRepository<SysRole, Long> {
-}
